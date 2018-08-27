71af8fb0158294fafc6e912988ae58ccf48fb900
Laura <lauraambrose@gmail.com>
2018-08-27 11:29:42 +0800

    ✨ feat(general): add API docs and refactor
    * 🚀 Added api comments
    
    * 🚀 Updated API doc comments
    
    * 🚀 Updated functions to use async await and added status codes
    
    * testing
    
    * added morgan
    
    * testing env vars
    
    * removed error line
    
    * removed players from url
    
    * removed all reference to 500 errors
    
    * Fleshed out calls and added get all call
    
    * updated route descriptions
    
    * added console logs for errors
    
    * 🚀 Updates after Nadeems PR comments
    
    * Refactored some of the controller.
    
    * updates after PR comments
    
    * Updates after PR comments
    
    * updated setting the port to be 8000 or env variable
    
    * updated error messages

--- routes/player.route.js
+++ routes/player.route.js
@@ -1,16 +1,52 @@
 const express = require('express')
 const router = express.Router()
 
-const player_controller = require('../controllers/player.controller')
+const playerController = require('../controllers/player.controller')
 
-router.get('/hello', player_controller.hello)
+/**
+ * @api {post} /create Creates player
+ * @apiName createUser
+ * @apiParam {String} name Players escaped slack username.
+ * @apiParam {Number} score Users score, sent by default at 0 on creation.
+ * @apiSuccess {Object} response Information about the player that was created
+ * @apiError {String} error.name there was a problem retrieving data from the database
+ */
+router.post('/create', playerController.playerCreate)
 
-router.post('/create', player_controller.player_create)
+/**
+ * @api {get} /get/{username} Gets the requested player
+ * @apiName getUser
+ * @apiParam (Url) {String} id Users unique id.
+ * @apiSuccess {Object} returns players details
+ * @apiError (404) {String} error.username player not found
+ * @apiError {String} error.username there was a problem retrieving data from the database
+ */
+router.get('/get/:username', playerController.playerDetails)
 
-router.get('/:id', player_controller.player_details)
+/**
+ * @api {put} /{username}/update Updates the players details
+ * @apiName updateUserDetails
+ * @apiParam (Url) {String} username Users slack username.
+ * @apiSuccess {Object} returns updated player
+ * @apiError {String} error.username there was a problem retrieving data from the database
+ */
+router.put('/:username/update', playerController.playerUpdate)
 
-router.put('/:id/update', player_controller.player_update)
+/**
+ * @api {delete} /{username}/delete Deletes the player
+ * @apiName deleteUser
+ * @apiParam (Url) {String} username Users name.
+ * @apiSuccess {String} message confirming player deleted
+ * @apiError {String} error.username player not deleted there was a problem retrieving data from the database
+ */
+router.delete('/:username/delete', playerController.playerDelete)
 
-router.delete('/:id/delete', player_controller.player_delete)
+/**
+ * @api {get} /all Gets a list of all the players
+ * @apiName getAllPlayers
+ * @apiSuccess {Object} returns details for all the players
+ * @apiError {String} error there was a problem retrieving data from the database
+ */
+router.get('/all', playerController.allPlayers)
 
 module.exports = router
